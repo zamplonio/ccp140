fa04d3e3ffda776d9e43eae35426d71c3ff26b3a
zamplonio <lucas.zamplonio@gmail.com>
2024-09-12 00:50:51 +0000

    adicionando classe Pessoa (setter n getters)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "Pessoa.cpp"
+using namespace std;
 
 int main(){
-    std::cout << "Hello world" << "\n";
+    Pessoa p;
+    p.setNome("Lucas");
+    p.setSobrenome("Zamplonio");
+    p.setCPF(121212121);
+
+    cout << "Dados da pessoa" << endl;
+    cout << "Nome:" << p.getNome() << " " << p.getSobrenome() << endl;
+    cout << "CPF" << p.getCPF() << endl;
 }   
